713e3eb41e523a131d30283a4cdb5c6ee67ee846
mona <heritageolaiy@gmail.com>
2025-01-28 14:54:50 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-[url(/img/heroBg.png)] bg-cover w-full h-[35rem]">
+        <nav class="border-2 border-red-600">
+            <!-- LOGO -->
+            <img src="/img/logo.svg" alt="" class="w-14 h-14">
+
+            <!-- NavItems -->
+             
+        </nav>
+    </div>
+  </template>
+  
