ecb32b30ab9ae5fca476c6806fa88a53d6ae7d10
mona <heritageolaiy@gmail.com>
2025-01-29 21:20:23 +0100

    working on the array to display the cards

--- components/TopProducts.vue
+++ components/TopProducts.vue
@@ -4,7 +4,21 @@
   
       <!-- Grid layout -->
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        
+        <div 
+          v-for="product in products" 
+          :key="product.name" 
+          class="bg-[url(/img/tp-bg.png)] bg-cover w-[11em] h-[14.5em] p-2"
+        >
+          <img :src="product.image" alt="" class="w-28 h-28 ml-6">
+          <div class="flex justify-between text-white text-sm mt-10">
+            <p>${{ product.price }}</p>
+            <Icon name="mdi:information-outline" />
+          </div>
+          <div class="flex justify-between text-white text-xs my-2">
+            <h1 class="w-10">{{ product.name }}</h1>
+            <button class="text-white bg-red-800 h-8 px-2">Add</button>
+          </div>
+        </div>
       </div>
     </div>
   </template>
