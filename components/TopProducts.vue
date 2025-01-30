f719a9799a583f7b6d2ffe59d86e9708e53dca78
mona <heritageolaiy@gmail.com>
2025-01-30 14:44:58 +0100

    first commit of the day with working on the poroduct cards

--- components/TopProducts.vue
+++ components/TopProducts.vue
@@ -1,34 +1,39 @@
 <template>
-    <div class="border border-red-500 mt-14 mb-10 p-6">
-      <h1 class="text-center text-3xl italic font-bold my-5">Top Products</h1>
-  
-      <!-- Grid layout -->
-      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <div 
-          v-for="product in products" 
-          :key="product.name" 
-          class="bg-[url(/img/tp-bg.png)] bg-cover w-[11em] h-[14.5em] p-2"
-        >
-          <img :src="product.image" alt="" class="w-28 h-28 ml-6">
-          <div class="flex justify-between text-white text-sm mt-10">
+  <div class="border border-red-500 mt-14 mb-10 p-7 lg:px-28 w-full">
+    <h1 class="text-center text-3xl italic font-bold my-5">Top Products</h1>
+ 
+    <!-- Modified grid layout -->
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full justify-items-center">
+      <div 
+        v-for="product in products" 
+        :key="product.name" 
+        class="bg-[url(/img/tp-bg.png)] bg-cover w-[20rem] md:w-52 lg:w-[21.5em] h-[19.5em] p-2 flex flex-col justify-between rounded-2xl"
+      >
+        <div class="flex justify-center">
+          <img :src="product.image" :alt="product.name" class="lg:w-32 lg:h-32">
+        </div>
+        <div>
+          <div class="flex justify-between text-white text-sm">
             <p>${{ product.price }}</p>
             <Icon name="mdi:information-outline" />
           </div>
-          <div class="flex justify-between text-white text-xs my-2">
+          <div class="flex justify-between text-white text-xs mt-2">
             <h1 class="w-10">{{ product.name }}</h1>
             <button class="text-white bg-red-800 h-8 px-2">Add</button>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  const products = [
-    { image: "/img/peachDanish.svg", name: "Danish Peach", price: 40 },
-    { image: "/img/danishFruit.svg", name: "Butter Croissant", price: 35 },
-    { image: "/img/chocoMuffin.svg", name: "Choco Muffin", price: 50 },
-    { image: "/img/cinnamonRoll.svg", name: "Cinnamon Roll", price: 45 },
-  ];
-  </script>
-  
+  </div>
+ </template>
+
+<script setup>
+const products = [
+  { image: "/img/peachDanish.svg", name: "Danish Peach", price: 15 },
+  { image: "/img/danishFruit.svg", name: "Danish Fruit Bread", price: 35 },
+  { image: "/img/multi-grain.svg", name: "Multi Grain Bread", price: 50 },
+  { image: "/img/no_Knead.svg", name: "No Knead Bread", price: 40 },
+  { image: "/img/baklava.svg", name: "Baklava pastry", price: 60 },
+  { image: "/img/kaiserRoll.svg", name: "Kaiser Roll", price: 25 },
+];
+</script>
